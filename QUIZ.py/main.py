20f5cd2669b18eafa9a7c2313a28000a17718464
Sakthipoorani <sakthipoorani28@gmail.com>
2026-08-17 08:23:22 +0530

    Add files via upload

---
+++ QUIZ.py/main.py
@@ -0,0 +1,38 @@
+print('welcome to learn with esprit')
+player = input (' do you want to play? ')
+if player != "yes":
+    quit()
+print(' ok lets play')
+question = input("How many dayz do we have in a week? ")
+if question == "seven":
+    print('correct')
+else:
+    print('incorrect')
+
+question = input ("In which direction does the sun rise?")
+if question == "seven":
+    print('correct')
+
+else:
+    print('incorrect')
+
+question = input ("which is the fastest animal on the land?")
+if question == "cheetah":
+    print('correct')
+
+else:
+    print('incorrect')
+
+question = input ("what is our national bird?")
+if question == "Peacock":
+    print('correct')
+
+else:
+    print('incorrect')
+
+question = input ("Which is the largest ocean in the world?")
+if question == "pacific ocean":
+    print('correct')
+
+else:
+    print('incorrect')
